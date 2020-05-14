f3fedda208d964b35fa744d384c2791b916fd044
StarkHuu <45584377+StarkHuu@users.noreply.github.com>
2020-05-14 13:22:54 +0800

    Add files via upload

--- Arxiver/views.py
+++ Arxiver/views.py
@@ -8,6 +8,9 @@ import urllib
 import os
 from . import models
 import hashlib
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 from django.forms.models import model_to_dict
 '''import jaro'''
 import ast
@@ -563,22 +566,49 @@ def login(request):
 def register(request):
     userName = request.POST.get('userName', 'username')
     password = request.POST.get('password', 'xxx')
-    # email = request.POST.get('email', 'undefined')
+    email = request.POST.get('email', 'undefined')
+
+    my_sender = 'rg_firstgroup@163.com'  # 管理员邮箱账号
+    my_pass = 'GODKZOYOMBSPQJYI'  # 管理员邮箱授权码
+
 
     res = {'retCode': 0, 'message': ''}
 
     obj = models.UserModel.objects.filter(userName=userName)
-    if obj.count() == 0:
-        models.UserModel.objects.create(userName=userName, password=password,
-                                        collectList=['-1'], focusList=['-1'])
-        obj = models.UserModel.objects.get(userName=userName)
-        #obj.collectList.remove('-1')
-        obj.save()
-        res['retCode'] = 1
-        res['message'] = '注册成功'
+    objmail = models.UserModel.objects.filter(email=email)
+
+    if obj.count() == 0 and objmail.count() == 0:
+        mailret = True
+        try:
+            msg = MIMEText('您正在进行ArXiver注册，如果不是您亲自操作，请及时联系管理员邮箱', 'plain', 'utf-8')
+            msg['From'] = formataddr(["ArXiver管理员", my_sender])  # 发件人邮件昵称和账号
+            msg['To'] = formataddr(["注册用户", email])  # 收件人邮箱昵称和账号
+            msg['Subject'] = "ArXiver注册"  # 邮件标题
+            server = smtplib.SMTP_SSL("smtp.163.com", 465)  # SMTP服务器和端口
+            server.login(my_sender, my_pass)  # 发件人邮箱账号和密码
+            server.sendmail(my_sender, [my_user, ], msg.as_string())  # 发件人邮箱账号、收件人邮箱账号、发送邮件
+            server.quit()  # 关闭连接
+        except:
+            mailret = False
+
+        if mailret:
+            models.UserModel.objects.create(userName=userName, password=password,email=email,
+                                            collectList=['-1'], focusList=['-1'])
+
+            obj = models.UserModel.objects.get(userName=userName,email=email)
+            #obj.collectList.remove('-1')
+            obj.save()
+            res['retCode'] = 1
+            res['message'] = '注册成功'
+
+        else:
+            res['retCode'] = 2
+            res['message'] = '请输入正确的邮箱地址'
+        
     else:
         res['retCode'] = 0
-        res['message'] = '用户名已注册'
+        res['message'] = '用户名或邮箱已注册'
+
     return HttpResponse(json.dumps({'register': res}))
 
 
